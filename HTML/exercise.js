df3c7403275c03eb75db7f24c2865fdd6c8cfbfe
Zahran15 <164994790+Zahran15@users.noreply.github.com>
2024-03-29 19:12:22 +0700

    Add files via upload

---
+++ HTML/exercise.js
@@ -0,0 +1,9 @@
+function tampil() {
+    document.getElementById("demo").innerHTML="Tabel Akhir";
+    alert("Tabel berhasil diubah");
+}
+
+function tampil1() {
+    document.getElementById("demo").innerHTML="Tabel Awal";
+    alert("Tabel berhasil diubah");
+}
